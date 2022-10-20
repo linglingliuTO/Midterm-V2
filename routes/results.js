420f62afcb91f8e369e32d175a39f189b700f8fc
linglingliuTO <linglingliu.to@gmail.com>
2022-10-20 22:03:08 +0000

    cleanup format

--- routes/results.js
+++ routes/results.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const {getPollResults, getPollName} = require('../db/queries/results')
+const {getPollResults, getPollName} = require('../db/queries/getresults')
 const {getArray} = require('../server/helper')
 
 
